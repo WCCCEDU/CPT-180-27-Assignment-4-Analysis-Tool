f422a26ecbde56267f992a95343bd7f07955cff8
Zack Hall <hallzac@my.wccc.edu>
2016-03-09 14:17:11 -0500

    Tried some new things. May work?

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using std::getline;
 using std::ifstream;
 
 const int SIZE = 30;
+const int NUM_LETTERS = 26;
 string data_file_path="";
 
 int main(int argc, char *argv[]) {
@@ -17,7 +18,7 @@ int main(int argc, char *argv[]) {
     if(argv[1]){
         data_file_path= static_cast<string>(argv[1]);
 
-    } else if(argv[2]) {
+    } else if(static_cast<string>(argv[2]) == "Search") {
         if (argv[3]) {
             string search_word = static_cast<string>(argv[3]);
             char word[SIZE];
@@ -38,10 +39,94 @@ int main(int argc, char *argv[]) {
                 }
                 file.close();
             }
+        } else if (static_cast<string>(argv[2]) == "Sort") {
+            if (static_cast<string>(argv[3]) == "Ascending") {
+
+                string str, temp;
+                string strray[100];
+                int a, j;
 
 
+                ifstream file(data_file_path);
+
+                while (!file.eof()) {
+                    getline(file, str);
+                    cout << str << endl;
+                }
+                for (int i = 0; i < 99; i++) {
+                    getline(file, strray[i]);
+                    strray[i] = str;
+                }
+                for (j = 0; j < 98; j++) {
+                    for (a = 1; a < (100 - (j + 1)); a++) {
+                        if (strray[100 - a] < strray[100 - (a + 1)]) {
+                            temp = strray[100 - a];
+                            strray[100 - a] = strray[7 - (a + 1)];
+                            strray[100 - (a + 1)] = temp;
+                        }
+                    }
+                }
+            } else if (static_cast<string>(argv[3]) == "Descending") {
+
+                string str, temp;
+                string strray[100];
+                int a, j;
+
+
+                ifstream file(data_file_path);
+
+                while (!file.eof()) {
+                    getline(file, str);
+                    cout << str << endl;
+                }
+                for (int i = 0; i > 99; i--) {
+                    getline(file, strray[i]);
+                    strray[i] = str;
+                }
+                for (j = 0; j < 98; j++) {
+                    for (a = 1; a < (100 - (j + 1)); a++) {
+                        if (strray[100 - a] < strray[100 - (a + 1)]) {
+                            temp = strray[100 - a];
+                            strray[100 - a] = strray[7 - (a + 1)];
+                            strray[100 - (a + 1)] = temp;
+                        }
+                    }
+                }
+            }
+        } else if (static_cast<string>(argv[2]) == "Count") {
+            {
+                struct Letter_Stats {
+                    int count;
+                    char ch;
+                };
+                ifstream file(data_file_path);
+                char ch;
+                int i;
+                Letter_Stats stats[NUM_LETTERS];
+
+                file.open(data_file_path);
+                for (i = 0; i < NUM_LETTERS; i++) {
+                    stats[i].count = 0;
+                    stats[i].ch = ('a' + i);
+                }
+
+                while (!file.eof()) {
+                    file.get(ch);
+                    if ('a' <= ch && ch <= 'z')
+                        stats[ch - 'a'].count++;
+                    else if ('A' <= ch && ch <= 'Z')
+                        stats[ch - 'A'].count++;
+                }
+                file.close();
+
+                for (i = 0; i < NUM_LETTERS; i++) {
+                    if (stats[i].count > 0) {
+                        cout << stats[i].ch << " occurred " << stats[i].count << " times" << endl;
+                    }
+
+                }
+            }
         }
     }
-
     return 0;
 }
