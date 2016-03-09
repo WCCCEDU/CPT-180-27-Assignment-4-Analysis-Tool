cf8c71fe7187e7aff8a78b4b44162cde75c43788
Samantha Shurie <s_shurie@yahoo.com>
2016-03-09 16:06:13 -0500

    [WIP] still needs a lot of work

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#include <string.h>
 #include "analysis_tool.h"
+
+
 
 int main(int argc, char* argv[]) {
 
@@ -19,21 +22,32 @@ int main(int argc, char* argv[]) {
 
   arg1 = validateArg1(arg1);
   arg2 = validateArg2(arg2);
-  arg3 = validateArg3(arg2, arg3);
+  //arg3 = validateArg3(arg2, arg3);
 
-  // open file and prepare to start selected tool
-  ifstream file;
-  file.open(arg1);
+  int num_lines = getFileNumLines(arg1);
+  ifstream file(arg1);
 
-  // run tool depending on user selection
-  if(arg2 == "search"){
-    search(file, arg3);
-  }else if(arg2 == "sort"){
-    sort(file, arg3);
-  }else if(arg2 == "count"){
-    count(file);
+  file.seekg(0L, file.beg); // should return the read position to beginning of file
+
+  // Store lines in array of strings
+  string lines[num_lines];
+  for(int i = 0; i < num_lines; i++){
+    getline(file, lines[i]);
+    lines[i] = tokenize(lines[i]);
   }
 
+  file.close();
+
+  // run tool depending on user selection
+ // if(arg2 == "search"){
+  //  search(file, arg3);
+ // }else if(arg2 == "sort"){
+  //  sort(file, arg3);
+ // }else
+  //if(arg2 == "count"){
+    //count(file);
+  //}
+  //file.close();
   return 0;
 }
 
@@ -68,7 +82,7 @@ string validateArg2(string arg){
  * Argument 3 should provide a valid searchable item or order type upon return if the user chose either sort or
  * search for Argument 2
  */
-string validateArg3(string arg2, string arg3){
+/*string validateArg3(string arg2, string arg3){
   if(arg2 == "search"  || arg2 == "sort") {
     if (arg2 == "search") {
       while (arg3 == "") {
@@ -84,14 +98,73 @@ string validateArg3(string arg2, string arg3){
   }
   return arg3;
 }
+ */
 
-void search(ifstream file, string arg){}
-void sort(ifstream file, string arg){}
-void count(ifstream file){
-  const char letters[36] = {'a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+//void search(ifstream file, string arg){}
+//void sort(ifstream file, string arg){}
+/*void count(ifstream file){
+  const int SIZE = 36;
+  const char digits[SIZE] = {'a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
   't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-  int occurances[36];
+  int occurances[SIZE];  // parallel to digits
 
+  bool found;
+  int index = 0;
+  for(string line : lines){ // For each line in lines array
+
+    for(int current = 0; current < line.length(); current++){ // For each character in line
+      found = false;
+      while (found == false && index <= SIZE) {
+        if (line.at(current) == digits[index]) {
+          found = true;
+          occurances[index]++; //Add one to the number of occurances for found digit
+        }
+        index++;
+      }
+    }
+  }
+
+  for(int i = 0; i < SIZE; i++){
+    cout << digits[i] << "\t\t" << occurances[i] << "endl";
+  }
 }
+*/
+
+void tokenize(string line){
+
+  const int LENGTH = line.size();
+
+  char *tokenized = new char[LENGTH];
+  char *tokens = strtok((char *)line.c_str(), " ");
+
+  int count = 0;
+  while(tokens){
+    tokenized[count++] = *tokens;
+    tokens = strtok(NULL," ");
+  }
+
+  //Testing
+   for(int i = 0; i < LENGTH; i++){
+      cout << tokenized[i];
+   }
+}
+
+int ctoi(char character){
+  return character - '0';
+}
+
+int getFileNumLines(string path){
+  ifstream file(path);
+  string line;
+  int line_count = 0;
+  while(getline(file, line)){
+    ++line_count;
+  }
+  file.close();
+  return line_count;
+}
+
+
+
 
 
