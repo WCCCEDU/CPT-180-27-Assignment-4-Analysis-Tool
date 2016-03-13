196e332897cd0d5e2ea1821a2a17d70763e50693
Samantha Shurie <s_shurie@yahoo.com>
2016-03-13 17:06:49 -0400

    no message

--- analysis_tool.h
+++ analysis_tool.h
@@ -8,6 +8,7 @@
 using std::cout;
 using std::cin;
 using std::string;
+using std::endl;
 using std::iostream;
 using std::ifstream;
 
@@ -16,7 +17,8 @@ string validateArg2(string arg);
 string validateArg3(string arg2, string arg3);
 void tokenize(string line);
 int getFileNumLines(string path);
+
 int ctoi(char character);
 void search(ifstream file, string arg3);
 void sort(ifstream file, string arg3);
-void count(ifstream file);
+void count(string arg1, const int num_lines);
--- main.cpp
+++ main.cpp
@@ -5,49 +5,40 @@
 
 int main(int argc, char* argv[]) {
 
-  string arg1 = "";  // filepath
-  string arg2 = "";  // method provided
-  string arg3 = "";  // additional method provided depending on what arg2 method is called
+  string file_path = "";  // Arg 1
+  string method = "";  // method provided
+  string query = "";  // additional method provided depending on what method method is called
 
   // argc will be at least one automatically because arg[0] is equal to executable filepath
   if(argc > 1){
-    arg1 = static_cast<string>(argv[1]);
+    file_path = static_cast<string>(argv[1]);
     if(argc > 2) {
-      arg2 = static_cast<string>(argv[2]);
+      method = static_cast<string>(argv[2]);
       if (argc > 3) {
-        arg3 = static_cast<string>(argv[3]);
+        query = static_cast<string>(argv[3]);
       }
     }
   }
-
-  arg1 = validateArg1(arg1);
-  arg2 = validateArg2(arg2);
-  //arg3 = validateArg3(arg2, arg3);
-
-  int num_lines = getFileNumLines(arg1);
-  ifstream file(arg1);
+  
+  //check arguments for errors
+  file_path = validateArg1(file_path);
+  method = validateArg2(method);
+  query = validateArg3(method, query);
+  
+  //get the number of lines in the file
+  const int num_lines = getFileNumLines(file_path);
+  ifstream file(file_path);
 
   file.seekg(0L, file.beg); // should return the read position to beginning of file
 
-  // Store lines in array of strings
-  string lines[num_lines];
-  for(int i = 0; i < num_lines; i++){
-    getline(file, lines[i]);
-    lines[i] = tokenize(lines[i]);
-  }
-
-  file.close();
-
   // run tool depending on user selection
- // if(arg2 == "search"){
-  //  search(file, arg3);
- // }else if(arg2 == "sort"){
-  //  sort(file, arg3);
- // }else
-  //if(arg2 == "count"){
-    //count(file);
+  /*if(method == "search"){
+      search(file, query);
+  }else if(method == "sort"){
+    sort(file, query);
+   }else if(method == "count"){
+   */ count(file_path, num_lines);
   //}
-  //file.close();
   return 0;
 }
 
@@ -82,53 +73,71 @@ string validateArg2(string arg){
  * Argument 3 should provide a valid searchable item or order type upon return if the user chose either sort or
  * search for Argument 2
  */
-/*string validateArg3(string arg2, string arg3){
-  if(arg2 == "search"  || arg2 == "sort") {
-    if (arg2 == "search") {
-      while (arg3 == "") {
+string validateArg3(string method, string query){
+  if(method == "search"  || method == "sort") {
+    if (method == "search") {
+      while (query == "") {
         cout << "Query needed for Search Tool. Enter a string value to search for: ";
-        cin >> arg3;
+        cin >> query;
       }
-    } else {    //This else handles arg3 if "sort" is provided
-      while (arg3 != "asc" && arg3 != "desc") {
+    } else {    //This else handles query if "sort" is provided
+      while (query != "asc" && query != "desc") {
         cout << "Order type needed for Sort Tool. Enter either \"asc\" for ascending or \"desc\" for descending: ";
-        cin >> arg3;
+        cin >> query;
       }
     }
   }
-  return arg3;
+  return query;
 }
- */
 
 //void search(ifstream file, string arg){}
 //void sort(ifstream file, string arg){}
-/*void count(ifstream file){
+void count(string file_path, const int num_lines){
   const int SIZE = 36;
   const char digits[SIZE] = {'a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
   't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
   int occurances[SIZE];  // parallel to digits
+  // Initialize all elements of occurances to the value of zero
+  for(int i = 0; i < SIZE; i++ ){
+    occurances[i] = 0;
+  }
 
-  bool found;
-  int index = 0;
-  for(string line : lines){ // For each line in lines array
-
-    for(int current = 0; current < line.length(); current++){ // For each character in line
+  ifstream file(file_path);
+  string line;
+  while(getline(file, line)){
+    for(int current = 0; current < line.size(); current++){
+      bool found;
+      int index = 0;
       found = false;
-      while (found == false && index <= SIZE) {
-        if (line.at(current) == digits[index]) {
+      while(!found && index < SIZE) {
+        if(line.at(current) == digits[index]) {
           found = true;
-          occurances[index]++; //Add one to the number of occurances for found digit
+          occurances[index] += 1; //Add one to the number of occurances for found digit
         }
         index++;
       }
     }
   }
 
+  //Still needs file output
   for(int i = 0; i < SIZE; i++){
-    cout << digits[i] << "\t\t" << occurances[i] << "endl";
+    cout << digits[i] << "\t\t" << occurances[i] << endl;
   }
-}
+
+
+/*
+
+  // Store lines of file in array of strings
+  string lines[num_lines];
+  for(int i = 0; i < num_lines; i++){
+    getline(file, lines[i]);
+    // lines[i] = tokenize(lines[i]);
+  }
+
 */
+
+  file.close();
+}
 
 void tokenize(string line){
 
@@ -163,8 +172,3 @@ int getFileNumLines(string path){
   file.close();
   return line_count;
 }
-
-
-
-
-
