b13d3035a936342dc6ee556296b19750c192c2e0
Jonathan Chishko <chishko4@my.wccc.edu>
2016-03-23 21:36:43 -0400

    update count

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 
 
+
 using std::string;
 using std::ifstream;
 using std::ofstream;
@@ -64,7 +65,7 @@ void search(string search_item, ifstream *datafile, ofstream *output) {
 
         int pos = fileline.find(search_item);
         if(pos >=0) {
-            cout << "line: " << linenumber << " pos: " << pos << endl;
+            output << "line: " << linenumber << " pos: " << pos << endl;
         }
         linenumber++;
 
@@ -99,7 +100,8 @@ void sortasc(ifstream *datafile){
                 words[current_pos] = temp;
         }
         for (int i= 0; i < counter; i++) {
-           cout << words[i] << endl;
+            ofstream output;
+            output << words[i] << endl;
         }
         }
     }
@@ -131,27 +133,44 @@ void sortdesc(ifstream *datafile){
             words[current_pos] = temp;
         }
         for (int i = counter; i  >= 0 ; i--){
-            cout << words[i] << endl;
+            ofstream output;
+            output << words[i] << endl;
         }
         }
     }
 
 void count(ifstream *datafile) {
-    string fileline;
-    while (*datafile) {
-        getline(*datafile, fileline);
-        int length = fileline.size();
-        int *words = new int[length];
-        char *tokens = strtok((char *) fileline.c_str(), " ");
-        int counter = 0;
-        while (tokens) {
-            words[counter++] = ctoi(*tokens);
-            tokens = strtok(NULL, " ");
-        }
+    const int SIZE = 36;
+    char characters[SIZE] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                              "m", "n", "o", "p", "q", "r", "s", "t", "u", "v" , "w", "x", "y", "z",
+                                "0", "1", "2", "3", "4", "5", "6", "7" , "8", "9"};
+    int count[SIZE] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    char[36] = { "a" , "b" , "c" , "d"}
+    while(*!datafile.eof()){
+        char data_in_file;
+        datafile >> data_in_file;
+        for (int i = 0; i < SIZE - 1; i++){
+            if (data_in_file == characters[i]){
+                count[i]++;
+            }
+        }
+    }
+    for(int i = 0; i < SIZE - 1; i++){
+        for(int j = 0; j < SIZE - i - 1; j++){
+            if(count[i] > count[i + 1]){
+                int temp = count[j];
+                count[j] = count[j + 1];
+                count[j + 1] = temp;
+                char temp2 = characters[j];
+                characters[j] = characters[j + 1];
+                characters[j + 1] = temp2;
+        }
     }
 }
+    ofstream output;
+    output << "The most used character is " << characters[0] << " used " << count[0] << " times" << endl;
+    output << "The least used character is " << characters[SIZE - 1] << " used " << count[36] << " times" << endl;
 int ctoi (char c){
     return c - '0';
 }
